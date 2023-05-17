0350963a95293ef4fd2b81ea7f5525f9a9d70165
FelixJacobsen <felix.jacobsen@iths.se>
2023-05-17 15:18:44 +0200

    make hamburger-menu functional for small screens

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/FJ-logo.png";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div
       className="fixed w-full h-[80px] flex justify-between 
@@ -22,13 +25,16 @@ const NavBar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div className="md:hidden z-10">
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* Mobile menu*/}
       <ul
-        className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f]
-      flex flex-col justify-center items-center"
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
       >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
