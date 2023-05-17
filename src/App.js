8578472457facd884ba48a515a9d136f8b4ad3d5
FelixJacobsen <felix.jacobsen@iths.se>
2023-05-17 15:13:12 +0200

    create basic navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import NavBar from "./components/NavBar";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+      <h1 className="text-2xl font-bold">ee</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/FJ-logo.png";
+
+const NavBar = () => {
+  return (
+    <div
+      className="fixed w-full h-[80px] flex justify-between 
+    items-center px-4 bg-[#0a192f] text-gray-300"
+      text-gray-300
+    >
+      <div>
+        <img src={Logo} alt="Logo with FJ text" style={{ width: "50px" }}></img>
+      </div>
+
+      {/* MENU*/}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div className="md:hidden z-10">
+        <FaBars />
+      </div>
+      {/* Mobile menu*/}
+      <ul
+        className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f]
+      flex flex-col justify-center items-center"
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* m*/}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default NavBar;
