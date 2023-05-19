0cc8d54b7796dc421cddca1ebb8be85822581e93
FelixJacobsen <felix.jacobsen@iths.se>
2023-05-20 00:47:49 +0200

    Update text in About and Home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const About = () => {
   return (
@@ -7,7 +8,7 @@ const About = () => {
         <div className="max-w-[1000x] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-red-600">
-              About
+              About me
             </p>
           </div>
         </div>
@@ -15,24 +16,26 @@ const About = () => {
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-rioght text-4xl font-bold">
             <p>
-              Hello! I'm Felix, nice to meet you. This is my website to showcase
-              my skills and my personal projects
+              Hello! I'm Felix, This is my website to showcase my skills and my
+              personal projects
             </p>
           </div>
           <div>
             <p>
               I have recently graduated from a two-year program in Java
               development. The education was full-stack and has given me the
-              opportunity to work with both frontend and backend development.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              opportunity to work with backend as well as frontend.
+              <br />
+              <br />
+              All projects displayed on the website are created by me and are
+              showcased to demonstrate my programming skills.
+              <br />
+              <br />I am looking for opportunities in Java/React, if you are
+              looking for a committed developer, do not hesitate to{" "}
+              <Link className="cursor-pointer font-bold" to="contact">
+                contact
+              </Link>{" "}
+              me.
             </p>
           </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,8 @@ const Home = () => {
               I'm a Full Stack Developer
             </h2>
             <h2 className="text-[#8892b0] py-4 max-w-[700px]">
-              I'm a Full Stack Developer
+              I am a full-stack developer, specializing in backend development
+              with a focus on Java.
             </h2>
             <Link to="projects" smooth={true} offset={true} duration={500}>
               <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600">
