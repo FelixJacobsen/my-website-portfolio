2480d1673c39393e0780f5276ba62e9585c51e46
FelixJacobsen <felix.jacobsen@iths.se>
2023-05-27 17:27:03 +0200

    Small tailwind, index and app.js changes. Remove footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+
 function App() {
   return (
     <div>
@@ -14,7 +14,6 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
-      <Footer />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "text-red": "#F22E07",
+      },
+    },
   },
   plugins: [],
 };
